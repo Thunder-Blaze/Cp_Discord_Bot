cecde67dbe76c220d77b2d084f1f7400bde5987d
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-02-12 14:40:25 +0530

    Updated Puppeteer

--- commands/ccverify.js
+++ commands/ccverify.js
@@ -32,7 +32,9 @@ export default {
         await delay(60000)
 
         try {
-            const browser = await puppeteer.launch()
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            })
             const page = await browser.newPage();
             await page.goto(profileUrl, { waitUntil: 'networkidle2' });
             let stars = 'No ★';
