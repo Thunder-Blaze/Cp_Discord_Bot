0c025ec4cbea088ebe76e7a9e4c2f0516a1c30dd
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-02-15 22:22:00 +0530

    Fixed Browser Not Closing

--- commands/ccverify.js
+++ commands/ccverify.js
@@ -87,8 +87,8 @@ export default {
                 }
                 return results
             })
-
-            console.log(submissions)
+            
+            await browser.close();
 
             if (
                 submissions[0].verdict == true &&
